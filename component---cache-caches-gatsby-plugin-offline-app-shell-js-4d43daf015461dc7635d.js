754b02c576e89f41d7c75b2ca3de8b304847cbb9
badatt <badatt@users.noreply.github.com>
2025-04-19 11:13:46 +0000

    deploy: badatt/tidy-ui-docs@68cc24073d535d8b44dccca3f8c2978c045b4bd7

---
+++ component---cache-caches-gatsby-plugin-offline-app-shell-js-4d43daf015461dc7635d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktidy_ui_docs=self.webpackChunktidy_ui_docs||[]).push([[681],{7541:function(e,t,u){var n=u(4994);t.__esModule=!0,t.default=void 0;var r=n(u(6221)),a=n(u(6540)),d=function(e){function t(){return e.apply(this,arguments)||this}return(0,r.default)(t,e),t.prototype.render=function(){return a.default.createElement(a.default.Fragment,null)},t}(a.default.Component),l=d;t.default=l}}]);
+//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-4d43daf015461dc7635d.js.map
---
+++ component---src-pages-index-tsx-cb64e1a845250d0a4fff.js
@@ -0,0 +1,8 @@
+"use strict";(self.webpackChunktidy_ui_docs=self.webpackChunktidy_ui_docs||[]).push([[245],{2783:function(e,t,r){r.r(t);var a=r(6540),l=r(4794),n=r(9390),o=r(3489);const c=(0,n.styled)(n.Text.hero)`
+  ${e=>{let{theme:{palette:t}}=e;return n.css`
+    background: linear-gradient(to right, ${t.major[600]}, ${t.minor[600]});
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  `}}
+`;t.default=()=>{const{isDark:e}=(0,n.useTheme)();return a.createElement(o.YW,{path:"/Home"},a.createElement(n.FlexBox,{as:"section",fld:"column",padding:"4rem 0",gap:"2rem",ali:"center"},a.createElement(n.Text.h2,{bld:!0,ctr:!0,color:e?(0,n.hsla)(n.color.gray[300]):(0,n.hsla)(n.color.gray[700])},"Create elegant React apps with"),a.createElement(c,{bld:!0,ctr:!0},"Tidy UI"),a.createElement(n.Text.h5,{ctr:!0,width:"50%",color:(0,n.hsla)(n.color.slate[500])},"Lightweight, modular React component library, providing essential building blocks for effortless application development"),a.createElement(l.Link,{to:"/getting-started"},a.createElement(n.Button,{variant:"primary",girth:"xxl",tone:"success",padding:"0.5rem 4rem"},"Get started"))))}}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-cb64e1a845250d0a4fff.js.map
