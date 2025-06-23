27f3fbc736b480a4a6336ab254fbb3aedbccf187
badatt <badatt@users.noreply.github.com>
2025-06-23 19:27:38 +0000

    deploy: badatt/tidy-ui-docs@f0be4a3a8376640eee20e9acb4cda231acbfc3cf

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-518fd48d4f39c789d636.js"
+    "url": "webpack-runtime-70fc212e0b700d23c164.js"
   },
   {
     "url": "framework-373d336ed4581a281f52.js"
@@ -42,11 +42,11 @@ self.__precacheManifest = [
     "url": "f169d00e-4f5bc6842a3960e5e9b1.js"
   },
   {
-    "url": "app-b1d5e04f24e69c95602e.js"
+    "url": "app-82c3696439a858885ee4.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4798bf598afd0fc0e873e24a95b6350b"
+    "revision": "284b6c95da9926df0da88f517186bcf7"
   },
   {
     "url": "manifest.webmanifest",
@@ -160,7 +160,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-b1d5e04f24e69c95602e.js`))) {
+  if (!resources || !(await caches.match(`/app-82c3696439a858885ee4.js`))) {
     return await fetch(event.request)
   }
 
